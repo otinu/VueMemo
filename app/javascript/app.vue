d540452a59d3a98f3d234cb452d0470524ed12aa
otinu <fitshter@gmail.com>
2021-11-13 01:02:39 +0000

    [add]axios

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,14 +1,33 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <ul>
+      <li v-for="memo in memos" :key="memo.id">
+        {{ memo.title }}: {{ memo.description }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      memos: "memos"  //setMemo内のthis.memos に対応
+    }
+  },
+  mounted () {      //mounted()でアプリ起動のタイミングで関数を実行
+    this.setMemo();
+  },
+  methods: {
+
+    // 関数setMemoを定義
+    setMemo: function () {
+      axios.get('/api/memos') //axios.HTTPリクエスト(URL)で呼び出す
+      .then(response => (
+        this.memos = response.date
+        ))
     }
   }
 }
